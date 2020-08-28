403bf5a77ef227980401ea38351348826ea4dd6b
Bobbbay <abatterysingle@gmail.com>
2020-08-28 15:06:26 -0400

    Using d3-node, test output files (in build/)

--- src/wakatime-api/index.js
+++ src/wakatime-api/index.js
@@ -36,3 +36,14 @@ let today = https
   .end();
 
 console.log(today);
+
+const fs = require("fs");
+const output = require("d3node-output");
+const d3 = require("d3-node")().d3;
+const d3nPie = require("d3node-piechart");
+
+const csvString = fs.readFileSync("data/data.csv").toString();
+const data = d3.csvParse(csvString);
+
+// create output files
+output("./build/build", d3nPie({ data: data }));
