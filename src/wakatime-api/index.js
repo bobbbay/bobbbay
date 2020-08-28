c16f88b050820840903dd01ad03f61bc2de78865
Bobbbay <abatterysingle@gmail.com>
2020-08-28 16:12:20 -0400

    Modified code, potential working api

--- src/wakatime-api/index.js
+++ src/wakatime-api/index.js
@@ -1,6 +1,8 @@
 // Grab https
 const https = require("https");
 
+const { Parser } = require("json2csv");
+
 // Define a callback function that is constant throughout all requests
 const callback = function (response) {
   var str = "";
@@ -8,7 +10,41 @@ const callback = function (response) {
     str += chunk;
   });
   response.on("end", function () {
-    return str;
+    const fs = require("fs");
+
+    console.log(str);
+    /*
+    var json = JSON.parse(str).items;
+    console.log(json);
+    var fields = Object.keys(json[0]);
+    var replacer = function (key, value) {
+      return value === null ? "" : value;
+    };
+    var csv = json.map(function (row) {
+      return fields
+        .map(function (fieldName) {
+          return JSON.stringify(row[fieldName], replacer);
+        })
+        .join(",");
+    });
+    csv.unshift(fields.join(",")); // add header column
+    csv = csv.join("\r\n");
+    console.log(csv);
+
+    fs.writeFile("data/data.csv", csv, function (err) {
+      if (err) return console.log(err);
+      console.log("JSON > CSV > data/data.csv");
+    });
+*/
+    const output = require("d3node-output");
+    const d3 = require("d3-node")().d3;
+    const d3nPie = require("d3node-piechart");
+
+    const csvString = fs.readFileSync("data/data.csv").toString();
+    const data = d3.csvParse(csvString);
+
+    // create output files
+    output("./build/build", d3nPie({ data: str }));
   });
 };
 
@@ -34,16 +70,3 @@ let today = https
     callback
   )
   .end();
-
-console.log(today);
-
-const fs = require("fs");
-const output = require("d3node-output");
-const d3 = require("d3-node")().d3;
-const d3nPie = require("d3node-piechart");
-
-const csvString = fs.readFileSync("data/data.csv").toString();
-const data = d3.csvParse(csvString);
-
-// create output files
-output("./build/build", d3nPie({ data: data }));
