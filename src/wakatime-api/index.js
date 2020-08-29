f75879c4507cecea3fff1ae8d5a086944229ff68
Bobbbay <abatterysingle@gmail.com>
2020-08-29 15:10:12 -0400

    Finished wakatime module!

--- src/wakatime-api/index.js
+++ src/wakatime-api/index.js
@@ -11,8 +11,6 @@ const callback = function (response) {
   });
   response.on("end", function () {
     const fs = require("fs");
-
-    console.log(str);
     /*
     var json = JSON.parse(str).items;
     console.log(json);
@@ -36,7 +34,7 @@ const callback = function (response) {
       console.log("JSON > CSV > data/data.csv");
     });
 */
-    const output = require("d3node-output");
+    /*const output = require("d3node-output");
     const d3 = require("d3-node")().d3;
     const d3nPie = require("d3node-piechart");
 
@@ -44,7 +42,47 @@ const callback = function (response) {
     const data = d3.csvParse(csvString);
 
     // create output files
-    output("./build/build", d3nPie({ data: str }));
+    output("./build/build", d3nPie({ data: str }));*/
+
+    let chart = `<svg width="100%" height="100%" viewBox="0 0 42 42" class="donut">
+<circle class="donut-hole" cx="21" cy="21" r="15.91549430918954" fill="#fff"></circle>
+<circle class="donut-ring" cx="21" cy="21" r="15.91549430918954" fill="transparent" stroke="#d2d3d4" stroke-width="3"></circle>
+
+`;
+
+    str = JSON.parse(str);
+
+    let rawdata = fs.readFileSync("data/colors.json");
+    let parsed = JSON.parse(rawdata);
+    console.log(parsed);
+
+    allLength = 0;
+    color = "";
+    for (i = 0; i < str.data.languages.length; i++) {
+      console.log(str.data.languages[i]);
+      console.log(i);
+
+      try {
+        color = parsed[str.data.languages[i].name].color;
+      } catch {
+        color = "#000";
+      }
+
+      chart += `    <circle class="donut-segment" cx="21" cy="21" r="15.91549430918954" fill="transparent" stroke="${color}" stroke-width="3" stroke-dasharray="${
+        str.data.languages[i].percent
+      } ${100 - str.data.languages[i].percent}" stroke-dashoffset="${
+        100 - allLength + 25
+      }"></circle>
+      `;
+      allLength += str.data.languages[i].percent;
+    }
+
+    chart += `</svg>`;
+
+    fs.writeFile("build/build.svg", chart, function (err) {
+      if (err) return console.log(err);
+      console.log("Wrote to ./build/");
+    });
   });
 };
 
@@ -64,9 +102,10 @@ let last7Days = https
   )
   .end();
 
-let today = https
+/*let today = https
   .request(
     { host: "wakatime.com", path: "/api/v1/users/@bobbbay/stats/last_7_days" },
     callback
   )
   .end();
+*/
