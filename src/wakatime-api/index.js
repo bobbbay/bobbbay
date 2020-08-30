0cbdf293246654d470ebaf10f67fa16a86e6fadf
Bobbbay <abatterysingle@gmail.com>
2020-08-30 11:26:30 -0400

    Font changes to build.names.svg

--- src/wakatime-api/index.js
+++ src/wakatime-api/index.js
@@ -11,7 +11,7 @@ const callback = function (response) {
   });
   response.on("end", function () {
     const fs = require("fs");
-    
+
     let chart = `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 42 42" class="donut">
 <circle class="donut-hole" cx="21" cy="21" r="15.91549430918954" fill="#fff"></circle>
 <circle class="donut-ring" cx="21" cy="21" r="15.91549430918954" fill="transparent" stroke="#d2d3d4" stroke-width="3"></circle>
@@ -60,10 +60,15 @@ const callback = function (response) {
 
     for (i = 0; i < usedLanguages.length; i++) {
       colorNames += `
-        <rect width="1" height="1" style="fill:${usedColors[i]};" y="${
-        i + 0.1
-      }" />
-        <text font-size="1" x="1.2" y="${i + 1}">${usedLanguages[i]}</text>
+        <rect 
+        width="1" height="1" 
+        style="fill:${usedColors[i]};" y="${i + 0.1}" />
+        <text 
+          font-family="Arial, Helvetica, sans-serif" 
+          font-size="1" 
+          x="1.2" y="${i + 1}">
+          ${usedLanguages[i]}
+        </text>
       `;
     }
 
