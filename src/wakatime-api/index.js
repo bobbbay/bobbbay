f6b1cc9ecbf1e1587cf4dd48156c79e7beafb667
Bobbbay <abatterysingle@gmail.com>
2020-08-30 11:20:58 -0400

    Update index.js

--- src/wakatime-api/index.js
+++ src/wakatime-api/index.js
@@ -11,43 +11,10 @@ const callback = function (response) {
   });
   response.on("end", function () {
     const fs = require("fs");
-    /*
-    var json = JSON.parse(str).items;
-    console.log(json);
-    var fields = Object.keys(json[0]);
-    var replacer = function (key, value) {
-      return value === null ? "" : value;
-    };
-    var csv = json.map(function (row) {
-      return fields
-        .map(function (fieldName) {
-          return JSON.stringify(row[fieldName], replacer);
-        })
-        .join(",");
-    });
-    csv.unshift(fields.join(",")); // add header column
-    csv = csv.join("\r\n");
-    console.log(csv);
-
-    fs.writeFile("data/data.csv", csv, function (err) {
-      if (err) return console.log(err);
-      console.log("JSON > CSV > data/data.csv");
-    });
-*/
-    /*const output = require("d3node-output");
-    const d3 = require("d3-node")().d3;
-    const d3nPie = require("d3node-piechart");
-
-    const csvString = fs.readFileSync("data/data.csv").toString();
-    const data = d3.csvParse(csvString);
-
-    // create output files
-    output("./build/build", d3nPie({ data: str }));*/
-
+    
     let chart = `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 42 42" class="donut">
 <circle class="donut-hole" cx="21" cy="21" r="15.91549430918954" fill="#fff"></circle>
 <circle class="donut-ring" cx="21" cy="21" r="15.91549430918954" fill="transparent" stroke="#d2d3d4" stroke-width="3"></circle>
-
 `;
 
     str = JSON.parse(str);
@@ -127,11 +94,3 @@ let last7Days = https
     callback
   )
   .end();
-
-/*let today = https
-  .request(
-    { host: "wakatime.com", path: "/api/v1/users/@bobbbay/stats/last_7_days" },
-    callback
-  )
-  .end();
-*/
