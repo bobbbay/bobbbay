4d5caae8437e08cb99f2959ae809ba1f42e4c37f
Bobbbay <abatterysingle@gmail.com>
2020-08-30 10:37:53 -0400

    Smaller viewbox for build.names.svg

--- src/wakatime-api/index.js
+++ src/wakatime-api/index.js
@@ -89,7 +89,7 @@ const callback = function (response) {
       console.log("Wrote to ./build/");
     });
 
-    let colorNames = `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 42 42">`;
+    let colorNames = `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 10 10">`;
 
     for (i = 0; i < usedLanguages.length; i++) {
       colorNames += `
