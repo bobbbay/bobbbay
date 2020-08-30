b48dd2b1b3a8396f86668192ff269b34a4ab3622
Bobbbay <abatterysingle@gmail.com>
2020-08-30 11:36:11 -0400

    Formatted SVGs and prettier JS

--- src/wakatime-api/index.js
+++ src/wakatime-api/index.js
@@ -1,8 +1,6 @@
 // Grab https
 const https = require("https");
 
-const { Parser } = require("json2csv");
-
 // Define a callback function that is constant throughout all requests
 const callback = function (response) {
   var str = "";
@@ -13,24 +11,20 @@ const callback = function (response) {
     const fs = require("fs");
 
     let chart = `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 42 42" class="donut">
-<circle class="donut-hole" cx="21" cy="21" r="15.91549430918954" fill="#fff"></circle>
-<circle class="donut-ring" cx="21" cy="21" r="15.91549430918954" fill="transparent" stroke="#d2d3d4" stroke-width="3"></circle>
+  <circle class="donut-hole" cx="21" cy="21" r="15.91549430918954" fill="#fff"></circle>
+  <circle class="donut-ring" cx="21" cy="21" r="15.91549430918954" fill="transparent" stroke="#d2d3d4" stroke-width="3"></circle>
 `;
 
     str = JSON.parse(str);
 
     let rawdata = fs.readFileSync("data/colors.json");
     let parsed = JSON.parse(rawdata);
-    console.log(parsed);
 
     allLength = 0;
     color = "";
     usedColors = [];
     usedLanguages = [];
     for (i = 0; i < str.data.languages.length; i++) {
-      console.log(str.data.languages[i]);
-      console.log(i);
-
       try {
         color = parsed[str.data.languages[i].name].color;
       } catch {
@@ -40,12 +34,16 @@ const callback = function (response) {
       usedColors.push(color);
       usedLanguages.push(str.data.languages[i].name);
 
-      chart += `    <circle class="donut-segment" cx="21" cy="21" r="15.91549430918954" fill="transparent" stroke="${color}" stroke-width="3" stroke-dasharray="${
-        str.data.languages[i].percent
-      } ${100 - str.data.languages[i].percent}" stroke-dashoffset="${
-        100 - allLength + 25
-      }"></circle>
-      `;
+      chart += `  <circle class="donut-segment" 
+    cx="21" cy="21" r="15.91549430918954" 
+    fill="transparent" stroke="${color}" 
+    stroke-width="3" 
+    stroke-dasharray="${str.data.languages[i].percent} ${
+        100 - str.data.languages[i].percent
+      }"
+    stroke-dashoffset="${100 - allLength + 25}">
+  </circle>
+`;
       allLength += str.data.languages[i].percent;
     }
 
@@ -53,34 +51,33 @@ const callback = function (response) {
 
     fs.writeFile("./build/build.svg", chart, function (err) {
       if (err) return console.log(err);
-      console.log("Wrote to ./build/");
     });
 
     let colorNames = `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 10 10">`;
 
     for (i = 0; i < usedLanguages.length; i++) {
       colorNames += `
-        <rect 
-        width="1" height="1" 
-        style="fill:${usedColors[i]};" y="${i + 0.1}" />
-        <text 
-          font-family="Arial, Helvetica, sans-serif" 
-          font-size="1" 
-          x="1.2" y="${i + 1}">
-          ${usedLanguages[i]}
-        </text>
-      `;
+  <rect 
+    width="1" height="1" 
+    style="fill:${usedColors[i]};" 
+    y="${i + 0.1}" 
+  />
+  <text 
+    font-family="Arial, Helvetica, sans-serif" 
+    font-size="1" 
+    x="1.2" y="${i + 1}">
+    ${usedLanguages[i]}
+  </text>
+`;
     }
 
     colorNames += `</svg>`;
 
     fs.writeFile("./build/build.names.svg", colorNames, function (err) {
       if (err) return console.log(err);
-      console.log("Wrote to ./build/ for names");
     });
 
-    console.log(usedColors);
-    console.log(usedLanguages);
+    console.log("Run successful.");
   });
 };
 
