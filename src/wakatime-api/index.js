397ed358ca3cefa0c527e5778c6b147328580f31
Bobbbay <abatterysingle@gmail.com>
2020-08-30 10:32:41 -0400

    Added building names svg

--- src/wakatime-api/index.js
+++ src/wakatime-api/index.js
@@ -44,7 +44,7 @@ const callback = function (response) {
     // create output files
     output("./build/build", d3nPie({ data: str }));*/
 
-    let chart = `<svg width="100%" height="100%" viewBox="0 0 42 42" class="donut">
+    let chart = `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 42 42" class="donut">
 <circle class="donut-hole" cx="21" cy="21" r="15.91549430918954" fill="#fff"></circle>
 <circle class="donut-ring" cx="21" cy="21" r="15.91549430918954" fill="transparent" stroke="#d2d3d4" stroke-width="3"></circle>
 
@@ -58,6 +58,8 @@ const callback = function (response) {
 
     allLength = 0;
     color = "";
+    usedColors = [];
+    usedLanguages = [];
     for (i = 0; i < str.data.languages.length; i++) {
       console.log(str.data.languages[i]);
       console.log(i);
@@ -67,6 +69,9 @@ const callback = function (response) {
       } catch {
         color = "#000";
       }
+
+      usedColors.push(color);
+      usedLanguages.push(str.data.languages[i].name);
 
       chart += `    <circle class="donut-segment" cx="21" cy="21" r="15.91549430918954" fill="transparent" stroke="${color}" stroke-width="3" stroke-dasharray="${
         str.data.languages[i].percent
@@ -79,10 +84,31 @@ const callback = function (response) {
 
     chart += `</svg>`;
 
-    fs.writeFile("build/build.svg", chart, function (err) {
+    fs.writeFile("./build/build.svg", chart, function (err) {
       if (err) return console.log(err);
       console.log("Wrote to ./build/");
     });
+
+    let colorNames = `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 42 42">`;
+
+    for (i = 0; i < usedLanguages.length; i++) {
+      colorNames += `
+        <rect width="1" height="1" style="fill:${usedColors[i]};" y="${
+        i + 0.1
+      }" />
+        <text font-size="1" x="1.2" y="${i + 1}">${usedLanguages[i]}</text>
+      `;
+    }
+
+    colorNames += `</svg>`;
+
+    fs.writeFile("./build/build.names.svg", colorNames, function (err) {
+      if (err) return console.log(err);
+      console.log("Wrote to ./build/ for names");
+    });
+
+    console.log(usedColors);
+    console.log(usedLanguages);
   });
 };
 
