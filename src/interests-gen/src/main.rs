ef7b00650e10285b7a0306743fbc1505216eddb4
Bobbbay <abatterysingle@gmail.com>
2020-10-27 08:55:06 -0400

    Add functionality (it works!)

--- src/interests-gen/src/main.rs
+++ src/interests-gen/src/main.rs
@@ -4,15 +4,23 @@ use rand::Rng;
 fn main() {
     let filename = "data/data.json";
 
-    let contents = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file");
+    let contents = "{
+  \"interests\": [
+    \"🍕 Pizza\",
+    \"🖥 Programming\",
+    \"🧠 Configuring Linux\",
+    \"📚 Reading\"
+  ]
+}
+"; //::read_to_string(filename)
+        //.expect("Something went wrong reading the file");
 
     let json: serde_json::Value =
         serde_json::from_str(&contents).expect("JSON was not well-formatted");
 
-    let mut rng = rand::thread_rng().gen_range(0, 1);
-    println("{}", rng);
+    let mut rng = rand::thread_rng().gen_range(0, 10);
+    println!("{}", rng);
 
     
-    println!("{}", json["interests"]);
+    println!("{}", json["interests"][rng]);
 }
